990b763c08829a38ae63be81e65884825bcd080a
Salma Qassem <salma99kasem@gmail.com>
2023-10-07 14:07:33 +0300

    upload project files

---
+++ src/App.jsx
@@ -0,0 +1,12 @@
+import { createBrowserRouter, RouterProvider } from "react-router-dom";
+
+const router = createBrowserRouter([
+  {
+    path: "/",
+  },
+]);
+function App() {
+  return <RouterProvider router={router} />;
+}
+
+export default App;
---
+++ src/main.jsx
@@ -0,0 +1,6 @@
+import ReactDOM from "react-dom/client";
+import App from "./App.jsx";
+import "./styles/main.scss";
+
+const root = ReactDOM.createRoot(document.getElementById("root"));
+root.render(<App />);
