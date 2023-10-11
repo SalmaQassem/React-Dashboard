345bb9f75d10ac37ed0d25bf27884837250ea3fa
Salma Qassem <salma99kasem@gmail.com>
2023-10-11 13:37:12 +0300

    edited inputs in contact page form

--- src/pages/Contract.jsx
+++ src/pages/Contract.jsx
@@ -3,6 +3,7 @@ import styles from "../styles/_Contract.module.scss";
 import StyledContainer from "../components/UI/StyledContainer";
 import logo from "../assets/images/logo.png";
 import image from "../assets/images/Frame.png";
+//import vector from "../assets/images/Vector2.png";
 import { SlUser } from "react-icons/sl";
 import { FaRegCalendarAlt } from "react-icons/fa";
 import FormButton from "../components/UI/FormButton";
@@ -46,49 +47,52 @@ const inputs = [
 
 const Contract = () => {
   return (
-    <StyledContainer>
-      <div className={styles.contract}>
-        <div className={styles.head}>
-          <h1>نموذج تعاقد</h1>
-          <div className={styles.logo}>
-            <img src={logo} alt="logo" />
-          </div>
-        </div>
-        <div className={styles.body}>
-          <div className={styles.image}>
-            <img src={image} alt="frame" />
-          </div>
-          <Form className={styles.form}>
-            <div className={styles.inputs}>
-              {inputs.map((item) => {
-                return item.type === "textArea" ? (
-                  <textarea
-                    key={item.id}
-                    className={styles.textArea}
-                    placeholder={item.placeholder}
-                  />
-                ) : (
-                  <div key={item.id} className={styles.input}>
-                    {item.type === "date" ? (
-                      <DateInput placeholder={item.placeholder}/>
-                    ) : (
-                      <input
-                        type={item.type}
-                        id={item.id}
-                        name={item.name}
-                        placeholder={item.placeholder}
-                      />
-                    )}
-                    <div className={styles.icon}>{item.icon}</div>
-                  </div>
-                );
-              })}
+    <>
+      <StyledContainer>
+        <div className={styles.contract}>
+          <div className={styles.head}>
+            <h1>نموذج تعاقد</h1>
+            <div className={styles.logo}>
+              <img src={logo} alt="logo" />
             </div>
-            <FormButton class={styles.button}>تصدير العقد</FormButton>
-          </Form>
+          </div>
+          <div className={styles.body}>
+            <div className={styles.image}>
+              <img src={image} alt="frame" />
+            </div>
+            <Form className={styles.form}>
+              <div className={styles.inputs}>
+                {inputs.map((item) => {
+                  return item.type === "textArea" ? (
+                    <textarea
+                      key={item.id}
+                      className={styles.textArea}
+                      placeholder={item.placeholder}
+                    />
+                  ) : (
+                    <div key={item.id} className={styles.input}>
+                      {item.type === "date" ? (
+                        <DateInput placeholder={item.placeholder} />
+                      ) : (
+                        <input
+                          type={item.type}
+                          id={item.id}
+                          name={item.name}
+                          placeholder={item.placeholder}
+                        />
+                      )}
+                      <div className={styles.icon}>{item.icon}</div>
+                    </div>
+                  );
+                })}
+              </div>
+              <FormButton class={styles.button}>تصدير العقد</FormButton>
+            </Form>
+          </div>
         </div>
-      </div>
-    </StyledContainer>
+      </StyledContainer>
+      {/*<img src={vector} alt="vector" className={styles.vector}/>*/}
+    </>
   );
 };
 
---
+++ src/pages/ReviewRoot.jsx
@@ -0,0 +1,9 @@
+import NavBar from "../components/NavBar/NavBar";
+const ReviewRoot = () => {
+  return (
+    <div>
+      <NavBar />
+    </div>
+  );
+};
+export default ReviewRoot;
