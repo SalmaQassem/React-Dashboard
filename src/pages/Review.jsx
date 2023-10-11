4b2022ca93ae8bf71d1c1e10ffdc43555ccaa391
Salma Qassem <salma99kasem@gmail.com>
2023-10-11 15:18:22 +0300

    added filter to contract page

--- src/pages/Review.jsx
+++ src/pages/Review.jsx
@@ -2,6 +2,11 @@ import styles from "../styles/_Review.module.scss";
 import StyledContainer from "../components/UI/StyledContainer";
 import ImagesGallery from "../components/UI/ImagesGallery";
 import { Link } from "react-router-dom";
+import { useEffect, useState } from "react";
+import { RiHotelLine } from "react-icons/ri";
+import { PiDoorOpenLight } from "react-icons/pi";
+import { FaKaaba } from "react-icons/fa6";
+import { AiOutlineFile, AiOutlineFileProtect } from "react-icons/ai";
 
 const infoItems = [
   { id: "0", name: "بيانات المنشأة" },
@@ -11,16 +16,58 @@ const infoItems = [
   { id: "4", name: "سجل المنشأة" },
 ];
 const data = [
-  { id: "0", name: "نوع المنشأة", value: "عمارة", category: "بيانات المنشأة" },
-  { id: "1", name: "عدد الغرف", value: "36 غرفة", category: "بيانات المنشأة" },
+  {
+    id: "0",
+    name: "نوع المنشأة",
+    value: "عمارة",
+    category: "بيانات المنشأة",
+    icon: <RiHotelLine />,
+  },
+  {
+    id: "1",
+    name: "عدد الغرف",
+    value: "36 غرفة",
+    category: "بيانات المنشأة",
+    icon: <PiDoorOpenLight />,
+  },
   {
     id: "2",
     name: "عدد الحجاج الفعلي",
     value: "43 حاج",
     category: "بيانات المنشأة",
+    icon: <FaKaaba />,
+  },
+  {
+    id: "3",
+    name: "عدد الحجاج في التصريح",
+    value: "33 حاج",
+    category: "بيانات المنشأة",
+    icon: <AiOutlineFile />,
+  },
+  {
+    id: "4",
+    name: "رقم التصريح",
+    value: "1877374718",
+    category: "بيانات المنشأة",
+    icon: <AiOutlineFileProtect />,
   },
 ];
 const Review = () => {
+  const [isActive, setIsActive] = useState("0");
+  const [filter, setFilter] = useState("بيانات المنشأة");
+  const [filteredData, setFilteredData] = useState([]);
+  const filterHandler = (e) => {
+    //console.log(e.target.id);
+    setIsActive(e.target.id);
+    setFilter(e.target.textContent);
+  };
+  useEffect(() => {
+    const filteredItems = data.filter((item) => {
+      return item.category === filter;
+    });
+    setFilteredData(filteredItems);
+  }, [filter]);
+
   return (
     <div className={styles.review}>
       <StyledContainer>
@@ -41,7 +88,16 @@ const Review = () => {
             <div className={styles.wrapper}>
               {infoItems.map((item) => {
                 return (
-                  <div key={item.id} className={styles.infoItem}>
+                  <div
+                    key={item.id}
+                    id={item.id}
+                    className={
+                      isActive === item.id
+                        ? `${styles.infoItem} ${styles.active}`
+                        : styles.infoItem
+                    }
+                    onClick={filterHandler}
+                  >
                     {item.name}
                   </div>
                 );
@@ -49,7 +105,26 @@ const Review = () => {
             </div>
           </StyledContainer>
         </div>
-        <div></div>
+        <div className={styles.data}>
+          <div className={styles.img} />
+          <StyledContainer>
+            <div className={styles.filteredData}>
+              {filteredData.length === 0 ? (
+                <p className={styles.message}>لا توجد بيانات</p>
+              ) : (
+                filteredData.map((item) => {
+                  return (
+                    <div key={item.id} className={styles.item}>
+                      <p className={styles.name}>{item.name}</p>
+                      <p className={styles.value}>{item.value}</p>
+                      <div className={styles.icon}>{item.icon}</div>
+                    </div>
+                  );
+                })
+              )}
+            </div>
+          </StyledContainer>
+        </div>
       </div>
     </div>
   );
